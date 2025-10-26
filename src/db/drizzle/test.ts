01c0f366af79f6f49a78e47cb11166442251a649
Westin9919 <viniciuswestin@hotmail.com>
2025-10-26 12:10:35 -0300

    att

--- src/db/drizzle/test.ts
+++ src/db/drizzle/test.ts
@@ -1,13 +1,8 @@
-// import { eq } from 'drizzle-orm';
 // import { drizzleDb } from '.';
 // import { postsTable } from './schemas';
-
 // (async () => {
-//   await drizzleDb
-//     .update(postsTable)
-//     .set({
-//       title: 'Rotina matinal de pessoas altamente eficazes',
-//       published: true,
-//     })
-//     .where(eq(postsTable.slug, 'rotina-matinal-de-pessoas-altamente-eficazes'));
+//   const posts = await drizzleDb.select().from(postsTable);
+//   posts.forEach(post => {
+//     console.log(post.slug);
+//   });
 // })();
